f21c66fbdda5d26757ba917474c87d1cc9943111
salambrosalam <romashkin1273@gmail.com>
2020-10-22 14:32:42 +0200

    awesome commit

---
+++ build/precache-manifest.4389e243d017e13d781afbb094eb2550.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a455de6fc3f247c4105743c38149c048",
+    "url": "/social_network/index.html"
+  },
+  {
+    "revision": "7400ab5d60998d657841",
+    "url": "/social_network/static/css/main.054cd10e.chunk.css"
+  },
+  {
+    "revision": "feafc29ef5c9c8db7902",
+    "url": "/social_network/static/js/2.8a005ab1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social_network/static/js/2.8a005ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7400ab5d60998d657841",
+    "url": "/social_network/static/js/main.764208c8.chunk.js"
+  },
+  {
+    "revision": "6ae66753bc494ba6f554",
+    "url": "/social_network/static/js/runtime-main.ff829eaf.js"
+  },
+  {
+    "revision": "dc9fc1469c19cb15c7ce2afb74d788c9",
+    "url": "/social_network/static/media/create_account_2_anonymous.dc9fc146.png"
+  },
+  {
+    "revision": "98ac1181715e2ee027e5aeb731a6fabf",
+    "url": "/social_network/static/media/hacker.98ac1181.jpg"
+  },
+  {
+    "revision": "51f78f244e75c19aa25ce83bd3469a33",
+    "url": "/social_network/static/media/loader_2.51f78f24.svg"
+  }
+]);
